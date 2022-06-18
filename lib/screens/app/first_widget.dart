8efced3ad61df09e8fc2ccd6f62f65ef25097d55
DanielMuyisa <danielmuyisa37@gmail.com>
2022-06-18 17:53:12 +0200

    updates

--- lib/screens/app/first_widget.dart
+++ lib/screens/app/first_widget.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:uzaboro/second_widget.dart';
+import 'package:uzaboro/screens/app/second_widget.dart';
 
 class FirstWidget extends StatelessWidget {
   const FirstWidget({Key? key}) : super(key: key);
