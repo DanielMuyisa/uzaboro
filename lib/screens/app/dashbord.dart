1a2e44942ba584506adb976c81d5158aeaba1d7f
DanielMuyisa <danielmuyisa37@gmail.com>
2022-06-18 17:39:08 +0200

    updates

--- lib/screens/app/dashbord.dart
+++ lib/screens/app/dashbord.dart
@@ -1,1 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
 
+class DashBord extends StatefulWidget {
+  const DashBord({Key? key}) : super(key: key);
+
+  @override
+  State<DashBord> createState() => _DashBordState();
+}
+
+class _DashBordState extends State<DashBord> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
